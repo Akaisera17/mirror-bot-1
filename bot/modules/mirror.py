bb7718525993c41f4b3cf3ac80b2ea245d183926
Akaisera17 <106355412+Akaisera17@users.noreply.github.com>
2022-07-12 14:44:40 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -200,7 +200,7 @@ class MirrorListener:
             msg += f'\n<b>Total Files: </b>{folders}'
             if typ != 0:
                 msg += f'\n<b>Corrupted Files: </b>{typ}'
-            msg += f'\n<b>👽 Hey </b>{self.tag} <b>Your Job is Done</b>\n<b>Thanks For Using This Bot</b>\n' /
+            msg += f'\n<b>👽 Request By </b>{self.tag} <b>Your Job is Done</b>\n<b>Thanks For using @Loidforgers_bot</b>\n'
             msg += f'\n<b>⏲ Elapsed Time:</b> {get_readable_time(time() - self.message.date.timestamp())}'
             if not files:
                 sendMessage(msg, self.bot, self.message)
@@ -219,7 +219,7 @@ class MirrorListener:
             if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
                 msg += f'\n<b>SubFolders: </b>{folders}'
                 msg += f'\n<b>Files: </b>{files}'
-            msg += f'\n<b>👽 Hey </b>{self.tag} <b>Your Job is Done</b>\n<b>Thanks For Using This Bot</b>\n' /
+            msg += f'\n\n<b>👽 Request By </b>{self.tag} <b>Your Job is Done</b>\n<b>Thanks For using @Loidforgers_bot</b>'
             msg += f'\n<b>⏲ Elapsed Time:</b> {get_readable_time(time() - self.message.date.timestamp())}'
             buttons = ButtonMaker()
             link = short_url(link)
